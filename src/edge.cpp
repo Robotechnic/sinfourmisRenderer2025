2161d4d75f337a2ae7323e33ecb3fa474cb7a127
Wyrdix <wyrdix@gmail.com>
2025-01-12 03:51:45 +0100

    Fix animations

--- src/edge.cpp
+++ src/edge.cpp
@@ -1,11 +1,12 @@
 #include "data_type.hpp"
 #include "dynamic_shape.hpp"
 
+#include <iostream>
+
 class Edge : public DynamicShape{
     public:
         const WorldData& world_;
         const EdgeData& data_;
-		const float thikness = 5;
 		sf::Vector2f from;
 		sf::Vector2f to;
 		float length;
@@ -17,23 +18,26 @@ class Edge : public DynamicShape{
     }
 
     void draw(sf::RenderWindow &window, const AnimationConfig &config, float t) override {
-		sf::RectangleShape line(sf::Vector2f(length, thikness));
+		sf::RectangleShape line(sf::Vector2f(length, config.line_thickness));
 		float lerped = std::lerp(0, 255, data_.life_ratio);
 		line.setFillColor(sf::Color(lerped, lerped, lerped));
-		line.rotate(atan2(to.y - from.y, to.x - from.x) * 180 / M_PI);
-		window.draw(line);
+		float angle = atan2(to.y - from.y, to.x - from.x);
+		line.rotate(angle  * 180 / M_PI);
+		
+		window.draw(line, config.graphic_transform*sf::Transform().translate(from)
+		.translate(std::sin(angle) * config.line_thickness/2, std::cos(angle) * config.line_thickness / 2));
 
 		for (auto &group: data_.groups) {
-			sf::CircleShape group_repr(0.5 * std::exp(-1 / group.group_data.qt));
+			float size = (config.node_size * 0.5 * std::exp(-1 / group.group_data.qt));
+			sf::CircleShape group_repr(size);
 			float progress = std::lerp(group.progress, group.anim_data->progress, t);
-			group_repr.setPosition(sf::Vector2f(
-				std::lerp(from.x, to.x, progress),
-				std::lerp(from.y, to.y, progress)
-			));
 			float transparency = (t <= 0.05 || t >= 0.95) ? std::exp(-4 * std::max(t, 1 - t)) : 1;
-			//group_repr.setFillColor(world_.teams.at(group.group_data.team).color);
+			group_repr.setFillColor(sf::Color((uint32_t) std::stoul(world_.teams.at(group.group_data.team).color.substr(1, 6)+"ff", nullptr, 16)));
 
-			window.draw(group_repr);
+			window.draw(group_repr, config.graphic_transform * sf::Transform().translate(sf::Vector2f(
+				std::lerp(from.x , to.x, progress) - size/2,
+				std::lerp(from.y, to.y, progress) - size/2
+			)));
 		}
 	};
 };
--- src/node.cpp
+++ src/node.cpp
@@ -1,21 +1,84 @@
 #include "data_type.hpp"
 #include "dynamic_shape.hpp"
+#include "primitives/arc.hpp"
+#include <iostream>
+#include <math.h>
 
 class Node : public DynamicShape {
-    public:
-        const WorldData& world_;
-        const NodeData& data_;
+  public:
+    const WorldData &world_;
+    const NodeData &data_;
+    std::vector<float> angle_before;
+    std::vector<float> angle_after;
+    std::vector<TeamData> teams_vector;
 
-    explicit Node(const WorldData& world, const NodeData& data ) : DynamicShape(5), world_(world), data_(data) {
-        
+    explicit Node(const WorldData &world, const NodeData &data)
+        : DynamicShape(5), world_(world), data_(data) {
+
+        std::map<int, int> before_map;
+        std::map<int, int> after_map;
+
+        int before_sum = 0;
+        int after_sum = 0;
+
+        for (const auto &[_, team] : world_.teams) {
+            before_map[team.id] = 0;
+            after_map[team.id] = 0;
+            teams_vector.push_back(team);
+        }
+
+        std::ranges::sort(teams_vector, [](auto &a, auto &b) { return a.id - b.id > 0; });
+
+        for (const auto &[_, group] : data_.ants) {
+            before_sum += group.qt;
+            before_map[group.team] += group.qt;
+        }
+
+        for (const auto &[_, group] :
+             data_.anim.value_or<NodeDataAnimation>({0, std::make_optional(data_.ants)})
+                 .ants.value_or(data_.ants)) {
+            after_sum += group.qt;
+            after_map[group.team] += group.qt;
+        }
+
+        const auto n = (int) world_.teams.size();
+        angle_after.resize(n);
+        angle_before.resize(n);
+
+        int acc_before = 0;
+        int acc_after = 0;
+
+        for (int i = 0; i < n; i++) {
+            angle_before[i + 1] = ((float)acc_before) / (float) std::max(before_sum, 1);
+            angle_after[i + 1] = ((float)acc_after) / (float) std::max(after_sum, 1);
+
+            acc_before += before_map[teams_vector.at(i).id];
+            acc_after += after_map[teams_vector.at(i).id];
+        }
+
+        angle_before[n + 1] = before_sum > 0 ? 1 : 0;
+        angle_after[n + 1] = after_sum > 0 ? 1 : 0;
     }
 
-    void draw(sf::RenderWindow &window, const AnimationConfig& config, float t) override {
-        sf::CircleShape circle(config.node_size);
+    void draw(sf::RenderWindow &window, const AnimationConfig &config, float t) override {
+        sf::CircleShape circle(config.node_size/2);
         circle.setFillColor(sf::Color::Black);
         circle.setOutlineColor(sf::Color::White);
         circle.setOutlineThickness(config.line_thickness);
 
-        window.draw(circle, config.graphic_transform * sf::Transform().translate(data_.pos_));
+        const auto n = (int) world_.teams.size();
+        for (int i = 0; i < n; i++) {
+            float angle_i = std::lerp(angle_before[i+1], angle_after[i+1], t) * 2 * M_PI;
+            float angle_i_p_1 = std::lerp(angle_before[i + 2], angle_after[i + 2], t) * 2 * M_PI;
+
+            Arc arc(config.node_size, angle_i, angle_i_p_1);
+            arc.setFillColor(
+                sf::Color((uint32_t) std::stoul(teams_vector.at(i).color.substr(1, 6)+"ff", nullptr, 16)));
+            arc.setThickness(10);
+            arc.updateArc();
+            window.draw(arc, config.graphic_transform * sf::Transform().translate(data_.pos_));
+        }
+
+        window.draw(circle, config.graphic_transform * sf::Transform().translate(data_.pos_).translate(-config.node_size/2.f, -config.node_size/2.f));
     }
 };
